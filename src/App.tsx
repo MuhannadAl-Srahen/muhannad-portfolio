67adfc8ec415011942f949536171f6c1d84783cb
Muhannad Al-Srahen <muhannadalsrahen@gmail.com>
2025-10-08 23:57:46 +0300

    Revert "Dev"

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import { ThemeProvider } from '@/contexts/ThemeContext'
 import Navbar from './components/Navbar'
 import Hero from '@/components/Hero'
 import About from '@/components/About'
-import Skills from './components/Skills'
+import Skills from '@/components/Skills'
+import Projects from '@/components/Projects'
+import Contact from '@/components/Contact'
 
 export default function App() {
   return (
@@ -13,6 +15,8 @@ export default function App() {
           <Hero />
           <About />
           <Skills />
+          <Projects />
+          <Contact />
         </main>
       </div>
     </ThemeProvider>
